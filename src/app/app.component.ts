9b25eb0f5175c864821c06a92d4a3fd11376b172
Joao Antonio dos Santos Ilario <joaoantonios74@gmail.com>
2019-02-14 23:47:53 -0300

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'futdati';
+  // atletas = sessionStorage.getItem('atletas');
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {FormsModule, NgForm} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ListarAtletasComponent } from './listar-atletas/listar-atletas.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CadastrarAtletaComponent } from './cadastrar-atleta/cadastrar-atleta.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomePageComponent},
+  { path: 'cadastrar', component: CadastrarAtletaComponent},
+  { path: 'listar', component: ListarAtletasComponent},
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarAtletasComponent,
+    PageNotFoundComponent,
+    HomePageComponent,
+    CadastrarAtletaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
